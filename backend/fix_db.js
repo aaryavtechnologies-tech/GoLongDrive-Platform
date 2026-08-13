4f230dff76755b9839a85896a7ed3dce66036d07
aaryavtechnologies-tech <aaryavtechnologies@gmail.com>
2026-08-13 12:51:25 +0530

    feat: add script to drop driverId_1 index from drivers collection

--- backend/fix_db.js
+++ backend/fix_db.js
@@ -1,3 +1,4 @@
+
 require('dotenv').config();
 const mongoose = require('mongoose');
 
@@ -6,7 +7,7 @@ mongoose.connect(process.env.MONGODB_URI).then(async () => {
         console.log("Connected. Dropping index 'driverId_1' on 'drivers' collection...");
         await mongoose.connection.collection('drivers').dropIndex('driverId_1');
         console.log('Index dropped successfully.');
-    } catch(err) {
+    } catch (err) {
         console.log("Error dropping index: ", err.message);
     }
     process.exit(0);
